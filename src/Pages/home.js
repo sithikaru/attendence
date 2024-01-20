8c5f40ae1ed76a2f2d004d2a313f2de8d3244ea5
Sithija Lakshan <sithikaru@gmail.com>
2024-01-20 13:44:01 +0530

    starting

---
+++ src/Pages/home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import app from '../config/firebase.config'
+function home() {
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
---
+++ src/config/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDq80jJAQmyx2Jyzn1Noh880Qrx5uv2N4s",
+  authDomain: "attendence-zijja.firebaseapp.com",
+  projectId: "attendence-zijja",
+  storageBucket: "attendence-zijja.appspot.com",
+  messagingSenderId: "321266866589",
+  appId: "1:321266866589:web:6d0f3ac61e55a447f33774"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
